57684c19b83bbb890991328aa3e5ba0b375f583c
Umut012 <79145797+Umut012@users.noreply.github.com>
2021-12-08 15:46:08 +0400

    Update bot-komutlar.js

--- komutlar/bot-komutlar.js
+++ komutlar/bot-komutlar.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js')
 exports.run = async (client, message, args, level) => {
     
     const embed = new Discord.MessageEmbed()
-    .setTitle(`Lord Creative - Komut Sayısı`)
-    .setDescription('**\n Lord Creative Bot | Toplam**  **`' + client.commands.size + '`** **Komut Vardır!**')
+    .setTitle(`Nether - Komut Sayısı`)
+    .setDescription('**\n Nether Bot | Toplam**  **`' + client.commands.size + '`** **Komut Vardır!**')
     .setColor("#ffffff")
     .setThumbnail('https://i.ibb.co/s2qGRFx/kod.png')
     .setTimestamp()
-    .setFooter("Lord Creative | Gelişmiş Türkçe Bot | 2020" , client.user.avatarURL())
+    .setFooter("Nether | Gelişmiş Türkçe Bot | 2021" , client.user.avatarURL())
 
     return message.channel.send(embed);
     
